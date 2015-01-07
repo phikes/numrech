6359c57c8638af98b499daef8b71cdf7f2981e36
Phillip Kessels <info@phikes.de>
2015-01-07 15:01:27 +0100

    Solution 2c now plots 2b in the coordinate system too

--- solution_2_c.m
+++ solution_2_c.m
@@ -1,8 +1,9 @@
-x = []
-err = []
+solution_2_b
+x2 = []
+y2 = []
 ex = [0.11183255915896; 0.11183255915896]
 for k = 1:1:10
-  x(end+1) = k;
-  y(end+1) = norm(abs(ex-newton(@func_2_c, @func_2_c_der, [0; 0], k)));
+  x2(end+1) = k;
+  y2(end+1) = norm(abs(ex-newton(@func_2_c, @func_2_c_der, [0; 0], k)));
 end
-semilogy(x, y)
+semilogy(x, y, x2, y2)
