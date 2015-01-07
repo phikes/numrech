be8d0ec02ac1ca0ecce2386bebac8f0e38fb6977
Phillip Kessels <info@phikes.de>
2015-01-07 14:55:47 +0100

    Added function for ex 2c

---
+++ func_2_c.m
@@ -0,0 +1,3 @@
+function res = func_2_c(vec)
+  res = [2/30*exp(vec(1))+1/30*exp(vec(2)); 1/30*exp(vec(1))+2/30*exp(vec(2))];
+end
---
+++ func_2_c_der.m
@@ -0,0 +1,3 @@
+function res = func_2_c_der(vec)
+  res = [2/30*exp(vec(1)); 2/30*exp(vec(2))];
+end
